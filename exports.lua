cc4e1d9900177413a692ecc638bce30830d5afeb
0xWaleed <imwaleed@outlook.sa>
2023-09-05 16:45:54 +0300

    style: fix indentation caused by the editor 🎨

--- exports.lua
+++ exports.lua
@@ -4,31 +4,31 @@
 ---
 
 function make_key_unique_for_resource(key)
-  local resource = GetInvokingResource()
-  return ('%s/%s'):format(resource, key)
+	local resource = GetInvokingResource()
+	return ('%s/%s'):format(resource, key)
 end
 
 exports('save', function(key, value)
-  ludb_save(make_key_unique_for_resource(key), value)
+	ludb_save(make_key_unique_for_resource(key), value)
 end)
 
 exports('saveGlobal', function(key, value)
-  ludb_save(('global/%s'):format(key), value)
+	ludb_save(('global/%s'):format(key), value)
 end)
 
 exports('retrieve', function(key)
-  return ludb_retrieve(make_key_unique_for_resource(key))
+	return ludb_retrieve(make_key_unique_for_resource(key))
 end)
 
 exports('retrieveGlobal', function(key)
-  return ludb_retrieve(('global/%s'):format(key))
+	return ludb_retrieve(('global/%s'):format(key))
 end)
 
 exports('delete', function(key)
-  ludb_delete(make_key_unique_for_resource(key))
+	ludb_delete(make_key_unique_for_resource(key))
 end)
 
 exports('deleteGlobal', function(key)
-  ludb_delete(('global/%s'):format(key))
+	ludb_delete(('global/%s'):format(key))
 end)
 
--- kvp_driver.lua
+++ kvp_driver.lua
@@ -5,22 +5,22 @@
 
 
 function kvp_driver()
-  local o = {}
+	local o = {}
 
-  function o.set(k, v)
-	SetResourceKvp(k, json.encode(v))
-  end
+	function o.set(k, v)
+		SetResourceKvp(k, json.encode(v))
+	end
 
-  function o.get(k)
-	local content = GetResourceKvpString(k)
-	return json.decode(content)
-  end
+	function o.get(k)
+		local content = GetResourceKvpString(k)
+		return json.decode(content)
+	end
 
-  function o.delete(k)
-	DeleteResourceKvp(k)
-  end
+	function o.delete(k)
+		DeleteResourceKvp(k)
+	end
 
-  return o
+	return o
 end
 
 ludb_set_driver(kvp_driver())
